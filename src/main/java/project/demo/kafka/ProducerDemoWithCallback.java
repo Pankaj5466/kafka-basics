7247e47e7be60c70356f500a430b96e90200d245
abc <abc@example.com>
2023-01-14 19:19:50 +0530

    send record in loop

--- src/main/java/project/demo/kafka/ProducerDemoWithCallback.java
+++ src/main/java/project/demo/kafka/ProducerDemoWithCallback.java
@@ -25,26 +25,31 @@ public class ProducerDemoWithCallback {
         //Step2: Create kafka producer
         KafkaProducer<String,String > producer = new KafkaProducer<String, String>(properties); //create producer with properties set above
 
-        //Step3: Create a producer record
-        ProducerRecord<String , String> producerRecord =
-                new ProducerRecord<>("demo_java","hello world gaya?");
 
-        //Step4: send data in async way
-        producer.send(producerRecord, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata metadata, Exception e) {
-                if(e == null){
-                    log.info("Received new metadata/ \n" +
-                            "Topic: " + metadata.topic() + "\n" +
-                            "Partition: " + metadata.partition() +"\n"+
-                            "Offset: " +  metadata.offset() + "\n" +
-                            "TimeStamp: "+ metadata.timestamp() + "\n");
+
+        // Send multiple records
+        for(int i=0;i<10;i++) {
+            //Step3: Create a producer record
+            ProducerRecord<String , String> producerRecord =
+                    new ProducerRecord<>("demo_java","hello world -> " + i);
+
+            //Step4: send data in async way
+
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception e) {
+                    if (e == null) {
+                        log.info("Received new metadata/ \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "TimeStamp: " + metadata.timestamp() + "\n");
+                    } else {
+                        log.error("Error While sending data to kafka\n");
+                    }
                 }
-                else {
-                    log.error("Error While sending data to kafka\n");
-                }
-            }
-        });
+            });
+        }
 
         //flush data (synchronous) //wait to send data
         producer.flush();
